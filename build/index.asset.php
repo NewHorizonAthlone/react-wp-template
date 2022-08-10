351790634b76b047e7eac267c770a084fe182096
martydotcom <56842363+martydotcom@users.noreply.github.com>
2022-08-10 11:51:44 +0100

    change component color (#1)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'f8534d29aa62673db815');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '0986feef92475699acaa');
